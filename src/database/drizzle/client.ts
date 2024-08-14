0f016e1b4de98bc7f1fe273655060c77e8cd4c5e
driback <driback00@outlook.com>
2024-08-14 18:34:34 +0700

    set max db connection to 1

--- src/database/drizzle/client.ts
+++ src/database/drizzle/client.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const createConnection = () => postgres(process.env.DATABASE_URL ?? "");
+const createConnection = () => postgres(process.env.DATABASE_URL ?? "", { max: 1 });
 
 const conn = createConnection();
 
